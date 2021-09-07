b7c4853fa17790245305ddbb46fac760c9a3ae0a
Amrutharaj <amrutha@zelarsoft.com>
2021-09-07 11:47:31 +0530

    ss

--- klusternetes-docs/docusaurus.config.js
+++ klusternetes-docs/docusaurus.config.js
@@ -5,7 +5,6 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 /** @type {import('@docusaurus/types').DocusaurusConfig} */
 (module.exports = {
   title: 'Kubernetes test clusters in seconds',
-  // tagline: 'Dinosaurs are cool',
   url: 'https://your-docusaurus-test-site.com',
   baseUrl: '/',
   onBrokenLinks: 'throw',
@@ -14,28 +13,28 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
   organizationName: 'zelarsoft', // Usually your GitHub org/user name.
   projectName: 'klusternetes', // Usually your repo name.
 
-  presets: [
-    [
-      '@docusaurus/preset-classic',
-      /** @type {import('@docusaurus/preset-classic').Options} */
-      ({
-        docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
-          editUrl: '',
-        },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          editUrl:
-            '',
-        },
-        theme: {
-          customCss: require.resolve('./src/css/custom.css'),
-        },
-      }),
-    ],
-  ],
+  // presets: [
+  //   [
+  //     '@docusaurus/preset-classic',
+  //     /** @type {import('@docusaurus/preset-classic').Options} */
+  //     ({
+  //       docs: {
+  //         sidebarPath: require.resolve('./sidebars.js'),
+  //         // Please change this to your repo.
+  //         editUrl: 'https://github.com/zs-amrutha/docusaurus/edit/main/website/',
+  //       },
+  //       blog: {
+  //         showReadingTime: true,
+  //         // Please change this to your repo.
+  //         editUrl:
+  //           'https://github.com/zs-amrutha/docusaurus/edit/main/website/blog/',
+  //       },
+  //       theme: {
+  //         customCss: require.resolve('./src/css/custom.css'),
+  //       },
+  //     }),
+  //   ],
+  // ],
 
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
@@ -54,7 +53,11 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
             label: 'Docs',
           },
           {to: '/blog', label: 'Blog', position: 'left'},
-
+          {
+            href: 'https://github.com/zs-amrutha/docusaurus',
+            label: 'GitHub',
+            position: 'right',
+          },
         ],
       },
       footer: {
@@ -73,6 +76,14 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
             title: 'Community',
             items: [
               {
+                label: 'Stack Overflow',
+                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+              },
+              {
+                label: 'Discord',
+                href: 'https://discordapp.com/invite/docusaurus',
+              },
+              {
                 label: 'Twitter',
                 href: 'https://twitter.com/docusaurus',
               },
@@ -85,10 +96,14 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
                 label: 'Blog',
                 to: '/blog',
               },
+              {
+                label: 'GitHub',
+                href: 'https://github.com/zs-amrutha/docusaurus',
+              },
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} klusternetes, Inc.`,
+        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
       },
       prism: {
         theme: lightCodeTheme,
