93736932cfe495a490aef5ec4634dcf4fbe92aed
Amrutharaj <amrutha@zelarsoft.com>
2021-09-07 13:21:45 +0530

    ss

--- klusternetes-docs/src/pages/index.js
+++ klusternetes-docs/src/pages/index.js
@@ -14,10 +14,11 @@ function HomepageHeader() {
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
-          <Link
+          {/* <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-          </Link>
+            Docusaurus Tutorial - 5min ⏱️
+          </Link> */}
         </div>
       </div>
     </header>
